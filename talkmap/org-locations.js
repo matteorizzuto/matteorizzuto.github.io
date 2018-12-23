741f67035a788bfdf00c8a6af58a721f0127dedd
Matteo Rizzuto <matteomrizzuto@gmail.com>
2018-12-23 13:46:28 -0500

    Edit way locations are coded in map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -3,11 +3,15 @@ var addressPoints = [
     "St. John's, Canada",
     47.561389,
     -52.7125,
+  ];
+  [
     "Guelph, Canada",
     43.533333,
     -80.223611,
+  ];
+  [
     "Biddeford, USA",
     43.458333,
     -70.3875,
-  ]
+  ];
   ];
