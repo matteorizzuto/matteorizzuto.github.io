e9dd00dd628f310181bcd179a2ba5e5fd808f9ab
Matteo Rizzuto <matteomrizzuto@gmail.com>
2018-12-23 13:52:54 -0500

    Edits to map and dates in teaching

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -3,11 +3,12 @@ var addressPoints = [
     "St. John's, Canada",
     47.561389,
     -52.7125,
-    "Guelph, Canada",
+  ],
+  ["Guelph, Canada",
     43.533333,
     -80.223611,
-    "Biddeford, USA",
+  ],
+  ["Biddeford, USA",
     43.458333,
-    -70.3875,
-  ]
+    -70.3875,]
   ];
