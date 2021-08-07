a337bad59b04c827bf091e414e16dbeb574cb02a
Matteo Rizzuto <matteomrizzuto@gmail.com>
2021-08-07 17:36:09 +0200

    Added a point to talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -4,11 +4,17 @@ var addressPoints = [
     47.561389,
     -52.7125,
   ],
-  ["Guelph, Canada",
+  [
+    "Guelph, Canada",
     43.533333,
     -80.223611,
   ],
-  ["Biddeford, USA",
+  [
+    "Biddeford, USA",
     43.458333,
-    -70.3875,]
+    -70.3875,],
+  [
+    "Long Beach, USA",
+    33.768321,
+    -118.195617,]
   ];
