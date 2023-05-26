93ae83708d070a13d9c3674907a80886885b29f9
Matteo Rizzuto <matteomrizzuto@gmail.com>
2023-05-26 16:22:04 -0400

    Added 2023 YPA Symposium talk to talk map and list

---
+++ _talks/2023_YPA_ZoogeochemModel.md
@@ -0,0 +1,9 @@
+---
+title: "<u>Rizzuto, M.<sup><a title='Presenting author'>†</a></sup></u>, Leroux, S. J., Schmitz, O. J. **Modeling the zoogeochemical effects of herbivores on ecosystem carbon cycles.**"
+collection: talks
+type: "Oral presentation"
+permalink: /talks/2023_YPA_ZoogeochemModel.md
+venue: "<i>6<sup>th</sup> Annual Symposium</i> Yale Postdoc Association"
+date: 2022-05-25
+location: "New Haven, USA"
+---
--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,24 +1,28 @@
 var addressPoints = [
   [
-    "Memorial University of Newfoundland, St. John's, Canada",
+    "Memorial University of Newfoundland, St. John's, NL, Canada",
     47.57203,
     -52.73265,
   ],
   [
-    "University of Guelph, Guelph, Canada",
+    "University of Guelph, Guelph, ON, Canada",
     43.533333,
     -80.223611,
   ],
   [
-    "University of New England, Biddeford, USA",
+    "University of New England, Biddeford, ME, USA",
     43.4582904,
     -70.3891151,],
   [
-    "Long Beach, USA",
+    "Long Beach, CA, USA",
     33.768321,
     -118.195617,],
   [
-    "University of Toronto-Scarborough, Scarborough, Canada",
+    "University of Toronto-Scarborough, Scarborough, ON, Canada",
     43.783611,
-    -79.185556,]
+    -79.185556,],
+  [
+    "Yale University, New Haven, CT, USA",
+    41.31,
+    -72.923611,]
 ];
